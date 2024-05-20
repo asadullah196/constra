e3dd1f1424195f62e448877d29bf98864acf0151
Asadullah Galib <md.asadullah196@gmail.com>
2024-05-20 16:05:52 +0800

    Search code fixed, working good in the home URL

--- inc/template-parts/header/header-search.php
+++ inc/template-parts/header/header-search.php
@@ -3,6 +3,21 @@
     <span id="search"><i class="fa fa-search"></i></span>
 </div><!-- Search block end -->
 
+
+
+<div class="search-block" style="display: none;">
+    <form action="<?php echo esc_url(home_url('/')); ?>" method="get">
+        <label for="search-field" class="w-100 mb-0">
+            <input type="text" name="s" class="form-control" id="search-field" placeholder="<?php echo esc_attr__('Type what you want and enter', 'constra'); ?>" value="<?php echo get_search_query(); ?>">
+        </label>
+        <span class="search-close">&times;</span>
+    </form>
+</div>
+
+<!-- Ssearch site end -->
+
+<?php /*
+
 <div class="search-block" style="display: none;">
     <form action="/" method="get">
         <label for="search search-field" class="w-100 mb-0">
@@ -11,9 +26,8 @@
         <span class="search-close">&times;</span>
     </form>
 </div>
-<!-- Ssearch site end -->
 
-<?php /*
+
  <!-- navigation drop search bar -->
  <div class="search" id="search-bar">
     <form role="search" class="search-form d-flex nav-search" action="/" method="get">
