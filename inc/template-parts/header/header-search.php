aba3338564f2d9fbb18dc6c506874552b4e69e06
Asadullah Galib <md.asadullah196@gmail.com>
2024-05-20 15:53:28 +0800

    Search file added

---
+++ inc/template-parts/header/header-search.php
@@ -0,0 +1,12 @@
+<!-- Search starts -->
+<div class="nav-search">
+    <span id="search"><i class="fa fa-search"></i></span>
+</div><!-- Search block end -->
+
+<div class="search-block" style="display: none;">
+    <label for="search-field" class="w-100 mb-0">
+    <input type="text" class="form-control" id="search-field" placeholder="Type what you want and enter">
+    </label>
+    <span class="search-close">&times;</span>
+</div>
+<!-- Ssearch site end -->
--- inc/template-parts/header/nav-menu.php
+++ inc/template-parts/header/nav-menu.php
@@ -78,17 +78,8 @@
             <!--/ Col end -->
         </div>
         <!--/ Row end -->
-
-        <div class="nav-search">
-            <span id="search"><i class="fa fa-search"></i></span>
-        </div><!-- Search end -->
-
-        <div class="search-block" style="display: none;">
-            <label for="search-field" class="w-100 mb-0">
-            <input type="text" class="form-control" id="search-field" placeholder="Type what you want and enter">
-            </label>
-            <span class="search-close">&times;</span>
-        </div><!-- Site search end -->
+        <!-- Search file calling -->
+        <?php get_template_part( 'inc/template-parts/header/header-search' ); ?>
     </div>
     <!--/ Container end -->
 </div>
