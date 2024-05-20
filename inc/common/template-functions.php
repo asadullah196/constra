db8fbd1b29e18293db9d46718916ddf1b52dbd30
Asadullah Galib <md.asadullah196@gmail.com>
2024-05-20 14:41:20 +0800

    Header-1 dynamic doe with case checked

--- inc/common/template-functions.php
+++ inc/common/template-functions.php
@@ -7,3 +7,17 @@ function constra_header(){
     get_template_part( 'inc/template-parts/header/top-bar' );
 	get_template_part( 'inc/template-parts/header/header-1' );
 }
+
+// Constra header section logo
+function constra_header_logo(){
+    $header_section_logo = get_theme_mod('constra_header_logo', get_template_directory_uri() . '/assets/images/Logo.png');
+	if( !empty($header_section_logo) ) : ?>
+        <a class="d-block" href="<?php echo esc_url(home_url('/')); ?>">
+            <img loading="lazy" src="<?php echo esc_url($header_section_logo); ?>" alt="Constra">
+        </a>
+	<?php else : ?>
+        <a class="d-block" href="<?php echo esc_url(home_url('/')); ?>">
+            <p><?php bloginfo('name'); ?></p>
+        </a>
+	<?php endif;	
+}
--- inc/template-parts/header/header-1.php
+++ inc/template-parts/header/header-1.php
@@ -1,44 +1,67 @@
+<?php
+
+    // header section
+    $header_section_phone_label = get_theme_mod('constra_header_calling_label', 'Call Us');
+    $header_section_phone_number = get_theme_mod('constra_header_calling_number', '(+9) 847-291-4353');
+    $header_section_email_label = get_theme_mod('constra_header_email_label', 'Email Us');
+    $header_section_email_address = get_theme_mod('constra_header_email_address', 'office@Constra.com');
+    $header_section_iso_label = get_theme_mod('constra_header_iso_label', 'Global Certificate');
+    $header_section_iso_number = get_theme_mod('constra_header_iso_number', 'ISO 9001:2017');
+    $header_section_button_text = get_theme_mod('constra_header_button_text', 'Get A Quote');
+    $header_section_button_url = get_theme_mod('constra_top_bar_button_url', '#');
+    
+?>
+
 <!-- Header start -->
 <header id="header" class="header-one">
     <div class="bg-white">
         <div class="container">
             <div class="logo-area">
                 <div class="row align-items-center">
+                <!-- logo start -->
                 <div class="logo col-lg-3 text-center text-lg-left mb-3 mb-md-5 mb-lg-0">
-                    <a class="d-block" href="index.html">
-                        <img loading="lazy" src="<?php echo esc_url(get_template_directory_uri()); ?>/assets/images/logo.png" alt="Constra">
-                    </a>
-                </div><!-- logo end -->
-
+                    <?php constra_header_logo(); ?>
+                </div>
+                <!-- logo end -->
                 <div class="col-lg-9 header-right">
                     <ul class="top-info-box">
+                        <?php if(!empty($header_section_phone_number)) : ?>
                         <li>
                             <div class="info-box">
                                 <div class="info-box-content">
-                                    <p class="info-box-title">Call Us</p>
-                                    <p class="info-box-subtitle">(+9) 847-291-4353</p>
+                                    <p class="info-box-title"><?php echo esc_html__($header_section_phone_label); ?></p>
+                                    <p class="info-box-subtitle"><?php echo esc_html__($header_section_phone_number); ?></p>
                                 </div>
                             </div>
                         </li>
+                        <?php endif; ?>
+                        <?php if(!empty($header_section_email_address)) : ?>
                         <li>
                             <div class="info-box">
                                 <div class="info-box-content">
-                                    <p class="info-box-title">Email Us</p>
-                                    <p class="info-box-subtitle">office@Constra.com</p>
+                                    <p class="info-box-title"><?php echo esc_html__($header_section_email_label); ?></p>
+                                    <p class="info-box-subtitle"><?php echo esc_html__($header_section_email_address); ?></p>
                                 </div>
                             </div>
                         </li>
+                        <?php endif; ?>
+                        <?php if(!empty($header_section_iso_number)) : ?>
                         <li class="last">
                             <div class="info-box last">
                                 <div class="info-box-content">
-                                    <p class="info-box-title">Global Certificate</p>
-                                    <p class="info-box-subtitle">ISO 9001:2017</p>
+                                    <p class="info-box-title"><?php echo esc_html__($header_section_iso_label); ?></p>
+                                    <p class="info-box-subtitle"><?php echo esc_html__($header_section_iso_number); ?></p>
                                 </div>
                             </div>
                         </li>
+                        <?php endif; ?>
+                        <?php if (!empty($header_section_button_text) && !empty($header_section_button_url)) : ?>
                         <li class="header-get-a-quote">
-                            <a class="btn btn-primary" href="contact.html">Get A Quote</a>
+                            <a class="btn btn-primary" href="<?php echo esc_url($header_section_button_url); ?>">
+                                <?php echo esc_html($header_section_button_text); ?>
+                            </a>
                         </li>
+                        <?php endif; ?>
                     </ul><!-- Ul end -->
                 </div><!-- header right end -->
                 </div><!-- logo area end -->
