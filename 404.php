c742ec3935a427bc52e237efb3d3133c2c55782e
Asadullah Galib <md.asadullah196@gmail.com>
2024-05-22 18:42:09 +0800

    404 page dynamic done

--- 404.php
+++ 404.php
@@ -8,29 +8,16 @@
  */
 ?>
 
+<?php
+    $error_page_heading = get_theme_mod('constra_error_page_heading','404');
+    $error_page_sub_heading = get_theme_mod('constra_error_page_sub_heading','Ops! PAGE NOT FOUND...');
+    $error_page_description = get_theme_mod('constra_error_page_description','Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere dolor voluptatum dolores veritatis provident reiciendis doloremque non autem soluta qui? Repudiandae, vel.');
+    $error_page_btn_text = get_theme_mod('constra_error_page_button_text','Back to Home');
+    $error_page_btn_url = get_theme_mod('constra_error_page_bar_button_url', '#');
+?>
+
 <!-- Calling header section -->
 <?php get_header(); ?>
-
-<div id="banner-area" class="banner-area" style="background-image:url(images/banner/banner1.jpg)">
-  <div class="banner-text">
-    <div class="container">
-        <div class="row">
-          <div class="col-lg-12">
-              <div class="banner-heading">
-                <h1 class="banner-title">Features</h1>
-                <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb justify-content-center">
-                      <li class="breadcrumb-item"><a href="#">Home</a></li>
-                      <li class="breadcrumb-item"><a href="#">Features</a></li>
-                      <li class="breadcrumb-item active" aria-current="page">404</li>
-                    </ol>
-                </nav>
-              </div>
-          </div><!-- Col end -->
-        </div><!-- Row end -->
-    </div><!-- Container end -->
-  </div><!-- Banner text end -->
-</div><!-- Banner area end --> 
 
 <section id="main-container" class="main-container">
   <div class="container">
@@ -40,14 +27,13 @@
       <div class="col-12">
         <div class="error-page text-center">
           <div class="error-code">
-            <h2><strong>404</strong></h2>
+            <h2><strong><?php echo esc_html__($error_page_heading, 'constra'); ?></strong></h2>
           </div>
           <div class="error-message">
-            <h3>Oops... Page Not Found!</h3>
+            <h3><?php echo esc_html__($error_page_sub_heading, 'constra'); ?></h3>
           </div>
-          <div class="error-body">
-            Try using the button below to go to main page of the site <br>
-            <a href="index.html" class="btn btn-primary">Back to Home Page</a>
+          <div class="error-body"><?php echo esc_html__($error_page_description, 'constra'); ?><br>
+            <a href="<?php echo esc_url($error_page_btn_url); ?>" class="btn btn-primary"><?php echo esc_html__($error_page_btn_text, 'constra'); ?></a>
           </div>
         </div>
       </div>
