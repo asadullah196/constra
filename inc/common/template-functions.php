dbae11a24e0f0c322948cec9db8c2c864a590c81
Asadullah Galib <md.asadullah196@gmail.com>
2024-05-22 13:57:02 +0800

    Footer top widgets updated

--- inc/common/template-functions.php
+++ inc/common/template-functions.php
@@ -74,15 +74,6 @@ function constra_primary_menus(){
 function constra_widgets_init() {
 
 	// Sidebar widget
-	register_sidebar(
-		array(
-			'name'          => esc_html__( 'Sidebar Widgets Area', 'constra' ),
-			'id'            => 'sidebar-widgets',
-			'description'   => esc_html__( 'Add sidebar widgets here.', 'constra' ),
-			'before_widget' => '<div id="%1$s" class="footer-widget-area footer-top-widget %2$s">',
-			'after_widget'  => '</div>',
-		)
-	);
 
 	// All footer widgets
 	register_sidebar(
@@ -90,8 +81,10 @@ function constra_widgets_init() {
 			'name'          => esc_html__( 'Footer 1 - Top Left', 'constra' ),
 			'id'            => 'footer-1',
 			'description'   => esc_html__( 'Add widgets here.', 'constra' ),
-			'before_widget' => '<div id="%1$s" class="footer-widget-area footer-top-widget %2$s">',
+			'before_widget' => '<div id="%1$s" class="col-lg-4 col-md-6 footer-widget footer-about %2$s">',
 			'after_widget'  => '</div>',
+            'before_title'  => '<h3 class="widget-title">',
+			'after_title'   => '</h3>',
 		)
 	);
 
@@ -100,8 +93,10 @@ function constra_widgets_init() {
 			'name'          => esc_html__( 'Footer 2 - Top Center', 'constra' ),
 			'id'            => 'footer-2',
 			'description'   => esc_html__( 'Add widgets here.', 'constra' ),
-			'before_widget' => '<div id="%1$s" class="footer-widget-area footer-center-widget %2$s">',
+			'before_widget' => '<div id="%1$s" class="col-lg-4 col-md-6 footer-widget mt-5 mt-md-0 %2$s">',
 			'after_widget'  => '</div>',
+            'before_title'  => '<h3 class="widget-title">',
+			'after_title'   => '</h3>',
 		)
 	);
 
@@ -110,8 +105,10 @@ function constra_widgets_init() {
 			'name'          => esc_html__( 'Footer 3 - Top Right', 'constra' ),
 			'id'            => 'footer-3',
 			'description'   => esc_html__( 'Add widgets here.', 'constra' ),
-			'before_widget' => '<div id="%1$s" class="footer-widget-area footer-bottom-widget %2$s">',
+			'before_widget' => '<div id="%1$s" class="col-lg-3 col-md-6 mt-5 mt-lg-0 footer-widget %2$s">',
 			'after_widget'  => '</div>',
+            'before_title'  => '<h3 class="widget-title">',
+			'after_title'   => '</h3>',
 		)
 	);
 
@@ -122,6 +119,8 @@ function constra_widgets_init() {
 			'description'   => esc_html__( 'Add widgets here.', 'constra' ),
 			'before_widget' => '<div id="%1$s" class="footer-widget-area footer-bottom-widget %2$s">',
 			'after_widget'  => '</div>',
+            'before_title'  => '<h3 class="widget-title">',
+			'after_title'   => '</h3>',
 		)
 	);
 }
