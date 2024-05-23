cea058b14dce453a09b5b846a5efba7cff329a76
Asadullah Galib <md.asadullah196@gmail.com>
2024-05-23 18:53:59 +0800

    Code optimized

--- index.php
+++ index.php
@@ -32,17 +32,9 @@ defined( 'ABSPATH' ) || exit;
         <?php else : ?>
           <?php echo get_template_part( 'inc/template-parts/content-none' ); ?>
         <?php endif; ?>
+        <br/>
         <!-- Blog pagination starts -->
         <?php constra_navigation(); ?>
-        <nav class="paging" aria-label="Page navigation example">
-            <ul class="pagination">
-            <li class="page-item"><a class="page-link" href="#"><i class="fas fa-angle-double-left"></i></a></li>
-            <li class="page-item"><a class="page-link" href="#">1</a></li>
-            <li class="page-item"><a class="page-link" href="#">2</a></li>
-            <li class="page-item"><a class="page-link" href="#">3</a></li>
-            <li class="page-item"><a class="page-link" href="#"><i class="fas fa-angle-double-right"></i></a></li>
-            </ul>
-        </nav>
         <!-- Blog pagination ends -->
       </div>
       <!-- Main blog area starts -->
