94bd6183330c6a8407367cf893b1505d4f8b509d
Asadullah Galib <md.asadullah196@gmail.com>
2024-06-02 14:30:40 +0800

    Page development going on.

--- page.php
+++ page.php
@@ -21,93 +21,20 @@
   <div class="container">
     <div class="row">
       <!-- Main blog area starts -->
-      <div class="col-lg-8 mb-5 mb-lg-0">
+      <div class="col-lg-1"></div>
+      <div class="col-lg-10 mb-5 mb-lg-0">
         <?php if ( have_posts() ) : ?>
           <?php while( have_posts()  ) : the_post(); ?>
-            <?php echo get_template_part( 'inc/template-parts/content' , get_post_format() ); ?>
+            <?php echo get_template_part( 'inc/template-parts/content-page'); ?>
           <?php endwhile; ?>
-        <?php else : ?>
-          <?php echo get_template_part( 'inc/template-parts/content-none' ); ?>
         <?php endif; ?>
         <br/>
         <!-- Blog pagination starts -->
         <?php constra_navigation(); ?>
         <!-- Blog pagination ends -->
       </div>
+      <div class="col-lg-1"></div>
       <!-- Main blog area starts -->
-      <!-- Side bar area starts -->
-      <div class="col-lg-4">
-        <div class="sidebar sidebar-right">
-          <div class="widget recent-posts">
-            <h3 class="widget-title">Recent Posts</h3>
-            <ul class="list-unstyled">
-              <li class="d-flex align-items-center">
-                <div class="posts-thumb">
-                  <a href="#"><img loading="lazy" alt="img" src="<?php echo get_template_directory_uri(); ?>/assets/images/news/news1.jpg"></a>
-                </div>
-                <div class="post-info">
-                  <h4 class="entry-title">
-                    <a href="#">We Just Completes $17.6 Million Medical Clinic In Mid-missouri</a>
-                  </h4>
-                </div>
-              </li><!-- 1st post end-->
-              <li class="d-flex align-items-center">
-                <div class="posts-thumb">
-                  <a href="#"><img loading="lazy" alt="img" src="<?php echo get_template_directory_uri(); ?>/assets/images/news/news2.jpg"></a>
-                </div>
-                <div class="post-info">
-                  <h4 class="entry-title">
-                    <a href="#">Thandler Airport Water Reclamation Facility Expansion Project Named</a>
-                  </h4>
-                </div>
-              </li><!-- 2nd post end-->
-              <li class="d-flex align-items-center">
-                <div class="posts-thumb">
-                  <a href="#"><img loading="lazy" alt="img" src="<?php echo get_template_directory_uri(); ?>/assets/images/news/news3.jpg"></a>
-                </div>
-                <div class="post-info">
-                  <h4 class="entry-title">
-                    <a href="#">Silicon Bench And Cornike Begin Construction Solar Facilities</a>
-                  </h4>
-                </div>
-              </li><!-- 3rd post end-->
-            </ul>
-          </div><!-- Recent post end -->
-          <div class="widget">
-            <h3 class="widget-title">Categories</h3>
-            <ul class="arrow nav nav-tabs">
-              <li><a href="#">Construction</a></li>
-              <li><a href="#">Commercial</a></li>
-              <li><a href="#">Building</a></li>
-              <li><a href="#">Safety</a></li>
-              <li><a href="#">Structure</a></li>
-            </ul>
-          </div><!-- Categories end -->
-          <div class="widget">
-            <h3 class="widget-title">Archives </h3>
-            <ul class="arrow nav nav-tabs">
-              <li><a href="#">Feburay 2016</a></li>
-              <li><a href="#">January 2016</a></li>
-              <li><a href="#">December 2015</a></li>
-              <li><a href="#">November 2015</a></li>
-              <li><a href="#">October 2015</a></li>
-            </ul>
-          </div><!-- Archives end -->
-          <div class="widget widget-tags">
-            <h3 class="widget-title">Tags </h3>
-            <ul class="list-unstyled">
-              <li><a href="#">Construction</a></li>
-              <li><a href="#">Design</a></li>
-              <li><a href="#">Project</a></li>
-              <li><a href="#">Building</a></li>
-              <li><a href="#">Finance</a></li>
-              <li><a href="#">Safety</a></li>
-              <li><a href="#">Contracting</a></li>
-              <li><a href="#">Planning</a></li>
-            </ul>
-          </div><!-- Tags end -->
-        </div>
-      </div><!-- Side bar area starts -->
     </div><!-- Main row end -->
   </div><!-- Container end -->
 </section>
