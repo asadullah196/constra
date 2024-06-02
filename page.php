552e22855b7010555946c74dfc73e222d0930c59
Asadullah Galib <md.asadullah196@gmail.com>
2024-06-02 14:27:23 +0800

    Template description updated

--- page.php
+++ page.php
@@ -1,20 +1,17 @@
 <?php
 /**
- * The main template file.
+ * The template for displaying all pages
  *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
  * @package constra
  * @since 1.0.0
  */
-
-// Exit if accessed directly.
-defined( 'ABSPATH' ) || exit;
 ?>
 
 <?php get_header(); ?>
