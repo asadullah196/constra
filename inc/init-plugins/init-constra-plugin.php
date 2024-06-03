f1270160936b07e82c5b28ec92bcfabc63089135
Asadullah Galib <md.asadullah196@gmail.com>
2024-06-03 11:31:08 +0800

    TGM initial setup done

--- inc/init-plugins/init-constra-plugin.php
+++ inc/init-plugins/init-constra-plugin.php
@@ -23,13 +23,13 @@
  * Depending on your implementation, you may want to change the include call:
  *
  * Parent Theme:
- * require_once get_template_directory() . '/path/to/tgm-plugin-activation.php';
+ * require_once get_template_directory() . '/path/to/class-tgm-plugin-activation.php';
  *
  * Child Theme:
- * require_once get_stylesheet_directory() . '/path/to/tgm-plugin-activation.php';
+ * require_once get_stylesheet_directory() . '/path/to/class-tgm-plugin-activation.php';
  *
  * Plugin:
- * require_once dirname( __FILE__ ) . '/path/to/tgm-plugin-activation.php';
+ * require_once dirname( __FILE__ ) . '/path/to/class-tgm-plugin-activation.php';
  */
 require_once get_template_directory() . '/inc/init-plugins/tgm-plugin-activation.php';
 
@@ -59,45 +59,20 @@ function constra_register_required_plugins() {
 	 */
 	$plugins = array(
 
-		// Required Plugin List
-		array(
-			'name'      => 'Kirki Customizer Framework',
-			'slug'      => 'kirki',
-			'required'  => true,
-		),
-
-		array(
-			'name'               => 'Advanced Custom Fields PRO',
-			'slug'               => 'advanced-custom-fields-pro',
-			'source'             => get_template_directory() . 'lib//plugins/advanced-custom-fields-pro.zip.zip',
-			'required'           => true,
-		),
-
-				// This is an example of how to include a plugin bundled with a theme.
 		array(
 			'name'               => 'Advanced Custom Fields PRO', // The plugin name.
-			'slug'               => 'tgm-example-plugin', // The plugin slug (typically the folder name).
-			'source'             => get_template_directory() . '/lib/plugins/tgm-example-plugin.zip', // The plugin source.
+			'slug'               => 'advanced-custom-fields', // The plugin slug (typically the folder name).
+			'source'             => get_template_directory() . '/inc/init-plugins/plugins/advanced-custom-fields-pro.zip', // The plugin source.
 			'required'           => true, // If false, the plugin is only 'recommended' instead of required.
-			'version'            => '', // E.g. 1.0.0. If set, the active plugin must be this version or higher. If the plugin version is higher than the plugin version installed, the user will be notified to update the plugin.
-			'force_activation'   => false, // If true, plugin is activated upon theme activation and cannot be deactivated until theme switch.
-			'force_deactivation' => false, // If true, plugin is deactivated upon theme switch, useful for theme-specific plugins.
-			'external_url'       => '', // If set, overrides default API URL and points to an external URL.
-			'is_callable'        => '', // If set, this callable will be be checked for availability to determine if a plugin is active.
 		),
 
-		// Recommended Plugin List
+		// This is an example of how to include a plugin from the WordPress Plugin Repository.
 		array(
-			'name'      => 'RankMath SEO',
-			'slug'      => 'seo-by-rank-math',
+			'name'      => 'Kirki Customizer Framework',
+			'slug'      => 'plugins/kirki',
 			'required'  => false,
 		),
 
-		array(
-			'name'      => 'Fluent Forms',
-			'slug'      => 'fluentform',
-			'required'  => false,
-		),
 	);
 
 	/*
